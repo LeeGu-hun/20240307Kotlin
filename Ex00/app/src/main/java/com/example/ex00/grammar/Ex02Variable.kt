d7dc0ac964019a0e8419d07cfe2326fd3aa2c34f
ihd12 <ihd+1@enbics.com>
2024-03-08 12:12:12 +0900

    Update Ex02Variable.kt

--- Ex00/app/src/main/java/com/example/ex00/grammar/Ex02Variable.kt
+++ Ex00/app/src/main/java/com/example/ex00/grammar/Ex02Variable.kt
@@ -22,14 +22,22 @@ fun main() {
     val num4 = 100
 //    num4 = 200  //val은 상수 선언시
 //    num5:Int = 10 //변수선언시 var 적용
-    var l1: Long = 1000
-    var i1: Int = 1000
-    var f1: Float = 0.1f
-    var d1: Double = 0.0
+    var b: Boolean = true
+    var c1: Char = ' ' // '\u0000'
+
     var b1: Byte = 100
     var s1: Short = 100
-    var c1: Char = ' ' // '\u0000'
-    var b: Boolean = true
+    var i1: Int = 1000
+    var l1: Long = 1000
+    println("Byte: ${Byte.MIN_VALUE}~${Byte.MAX_VALUE}")
+    println("Int: ${Int.MIN_VALUE}~${Int.MAX_VALUE}")
+    println("Short: ${Short.MIN_VALUE}~${Short.MAX_VALUE}")
+    println("Long: ${Long.MIN_VALUE}~${Long.MAX_VALUE}")
+
+    var f1: Float = 0.1f
+    var d1: Double = 0.0
+    println("Float: ${Float.MIN_VALUE}~${Float.MAX_VALUE}")
+    println("Double: ${Double.MIN_VALUE}~${Double.MAX_VALUE}")
 
     var ub: UByte = 10u
     var us: UShort = 10u
