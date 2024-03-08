26481dc3a8f8b0e98153973ea3238f72bf478f4d
ihd12 <ihd+1@enbics.com>
2024-03-08 10:43:21 +0900

    0308
    0308

---
+++ Ex00/app/src/main/java/com/example/ex00/grammar/Ex01VariableFile.kt
@@ -0,0 +1,9 @@
+package com.example.ex00.grammar
+
+fun main() {
+    println("Hello world")
+}
+
+fun p1(): String {
+    return "hello world"
+}
---
+++ Ex00/app/src/main/java/com/example/ex00/grammar/Ex01VariableJava.java
@@ -0,0 +1,7 @@
+package com.example.ex00.grammar;
+
+public class Ex01VariableJava {
+    public void printJava() {
+        System.out.println("Hello world");
+    }
+}
