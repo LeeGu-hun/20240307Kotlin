41a0b5b5094cc9aabbc42b81d4cf215ac19e700b
ihd12 <ihd+1@enbics.com>
2024-03-13 11:02:25 +0900

    313

---
+++ Ex00/app/src/main/java/com/example/ex00/RelativeActivity.kt
@@ -0,0 +1,14 @@
+package com.example.ex00
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RelativeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_relative)
+    }
+}
