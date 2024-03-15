155b9c0aa529b81b270045da670c377ff54a1014
ihd12 <ihd+1@enbics.com>
2024-03-15 11:13:12 +0900

    11

--- Ex00/app/src/main/java/com/example/ex00/ConstraintActivity.kt
+++ Ex00/app/src/main/java/com/example/ex00/ConstraintActivity.kt
@@ -1,11 +1,35 @@
 package com.example.ex00
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class ConstraintActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_constraint)
+        val btnMobile = findViewById<Button>(R.id.btnMobile)
+        btnMobile.setOnClickListener {
+            // 전화에 대한 권한 설정을 해줘야 함.
+            // 설정-애플리케이션-해당앱클릭-권한-전화를 on
+            // permission 을 별도로 설정해주려면 코드를 활용해야 함.
+            val tel = btnMobile.text
+            val uriPhoneNumber = Uri.parse("tel:" + tel)
+            val intent = Intent(Intent.ACTION_CALL, uriPhoneNumber)
+            startActivity(intent)
+        }
     }
 }
+
+
+
+
+
+
+
+
+
