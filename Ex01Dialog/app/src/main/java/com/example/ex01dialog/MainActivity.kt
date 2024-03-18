66a4a7481e527fe7b8fad2c94e19d412ac95abca
ihd12 <ihd+1@enbics.com>
2024-03-18 10:37:51 +0900

    318

---
+++ Ex01Dialog/app/src/main/java/com/example/ex01dialog/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.ex01dialog
+
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.ex01dialog.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+
+//        val btnToast = findViewById<Button>(R.id.btnToast)
+        val btnToast = binding.btnToast
+        btnToast.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                showToast()
+            }
+            Toast.makeText(this, "Toast 알림", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.R)
+    fun showToast(): Unit {
+        val toast = Toast.makeText(this, "Toast Callback", Toast.LENGTH_SHORT)
+        toast.addCallback(
+            object : Toast.Callback() {
+                override fun onToastShown() {
+                    super.onToastShown()
+                    Log.d(">>", "toast show")
+                }
+
+                override fun onToastHidden() {
+                    super.onToastHidden()
+                    Log.d(">>", "toast hidden")
+                }
+            }
+        )
+        toast.show()
+    }
+}
+
+
+
+
+
+
+
