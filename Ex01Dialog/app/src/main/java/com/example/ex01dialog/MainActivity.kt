cb6822910a9136b329d1aea328d491b30d26c51f
ihd12 <ihd+1@enbics.com>
2024-03-18 10:51:51 +0900

    Update MainActivity.kt

--- Ex01Dialog/app/src/main/java/com/example/ex01dialog/MainActivity.kt
+++ Ex01Dialog/app/src/main/java/com/example/ex01dialog/MainActivity.kt
@@ -16,8 +16,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
         val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.main)
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
 //            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -25,12 +25,15 @@ class MainActivity : AppCompatActivity() {
 //        }
 
 //        val btnToast = findViewById<Button>(R.id.btnToast)
-        val btnToast = binding.btnToast
-        btnToast.setOnClickListener {
+        binding.btnToast.setOnClickListener {
+            Log.d(">>", "btnToast")
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
                 showToast()
             }
             Toast.makeText(this, "Toast 알림", Toast.LENGTH_SHORT).show()
+        }
+        binding.btnNotify.setOnClickListener {
+            Log.d(">>", "btnNotify")
         }
     }
 
