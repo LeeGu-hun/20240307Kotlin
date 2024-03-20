0e12086f5a692e220c81e2cfa93ac90a17c14cf3
ihd12 <ihd+1@enbics.com>
2024-03-20 16:21:15 +0900

    0320

---
+++ Ex03Sqlite/app/src/main/java/com/example/ex03sqlite/ui/ListActivity.kt
@@ -0,0 +1,44 @@
+package com.example.ex03sqlite.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.ex03sqlite.R
+import com.example.ex03sqlite.adapter.MemberAdapter
+import com.example.ex03sqlite.dao.DbSqlHandler
+import com.example.ex03sqlite.databinding.ActivityListBinding
+
+class ListActivity : AppCompatActivity() {
+    // var binding: ActivityListBinding? = null
+    lateinit var binding: ActivityListBinding
+    lateinit var adapter: MemberAdapter
+    lateinit var sqlHandler: DbSqlHandler
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_list)
+        binding = ActivityListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        adapter = MemberAdapter()
+        sqlHandler = DbSqlHandler.open(applicationContext)
+
+        adapter.setList(sqlHandler.getList()) //adapter의 목록 초기화
+        binding.recycler.adapter = adapter //recycler에 adapter 적용
+
+        binding.recycler.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL,false)
+    }
+}
+
+
+
+
+
+
+
+
