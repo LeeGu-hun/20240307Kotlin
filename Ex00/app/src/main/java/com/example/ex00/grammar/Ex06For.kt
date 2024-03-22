dd50db97558edca09fa9100f737a3004b57ec11e
ihd12 <ihd+1@enbics.com>
2024-03-22 11:51:01 +0900

    Update Ex06For.kt

--- Ex00/app/src/main/java/com/example/ex00/grammar/Ex06For.kt
+++ Ex00/app/src/main/java/com/example/ex00/grammar/Ex06For.kt
@@ -1,6 +1,26 @@
 package com.example.ex00.grammar
 
 fun main() {
+    // 미만일 때 0~9
+    for (i in 0 until (10)) {
+        println(i)
+    }
+
+    // 포함일 때
+    for (i in 1..9) {
+        println(i)
+    }
+
+    // step 증가
+    for (i in 1..10 step (3)) {
+        println(i)
+    }
+
+    // 감소할 때
+    for (i in 10 downTo (1) step (1)) {
+        println(i)
+    }
+
     var sum = 0
     for (i in 1..10) {
         sum += i
@@ -9,15 +29,15 @@ fun main() {
 
     val arr:IntArray = intArrayOf(1,2,3,4,5)
     // 배열의 인덱스와 원소를 다룰 때
-    for ((i, value) in arr.withIndex()) println("arr[$i] : $value ")
+    for ((i, value) in arr.withIndex()) println(" arr[$i] : $value ")
+    for (i in 0..arr.size) println(" 인덱스 : $i / 값 : arr[$i] ")
 
     // 배열의 인덱스를 다룰 때 indices
     for (i in arr.indices) println(i)
 
     // 배열의 원소만 다룰 때
     for (i in arr) println(i)
-
-    for (i in 10 downTo (1) step (1)) {
-        println(i)
-    }
 }
+
+
+
