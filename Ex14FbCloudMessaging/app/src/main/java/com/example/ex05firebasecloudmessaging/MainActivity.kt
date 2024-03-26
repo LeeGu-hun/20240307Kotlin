8d6991dfc97dcab98ec8fc5829283171f6d4e426
ihd12 <ihd+1@enbics.com>
2024-03-26 15:39:27 +0900

    still unfinished

--- Ex14FbCloudMessaging/app/src/main/java/com/example/ex05firebasecloudmessaging/MainActivity.kt
+++ Ex14FbCloudMessaging/app/src/main/java/com/example/ex05firebasecloudmessaging/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.ex05firebasecloudmessaging.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    // Not Yet Sorry~!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
